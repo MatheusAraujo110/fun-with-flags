ea7d891e5885177c2bb0245550b2d8f480e8427b
Matheus Araujo <theusnascimento386@gmail.com>
2025-03-05 20:56:15 -0300

    add select for country regions

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Card, Grid, Search } from "./components"
+import { Card, Grid, Search, Select } from "./components"
 import { countriesApi } from "./services"
 import { useEffect, useState } from "react"
 import Link from "next/link"
@@ -23,6 +23,7 @@ export default function Home() {
     const [serach, setSearch] = useState("")
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState<string | null>(null)
+    const [selected, setSelected] = useState("All regions")
 
     useEffect(() => {
         const fetchCountries = async () => {
@@ -42,18 +43,29 @@ export default function Home() {
     if (loading) return <div>loading...</div>
     if (error) return <div>{error}</div>
 
+    const regions = [... new Set(countries.map(({ region }) => region))]
+
     const sortedCountries = countries.sort((a, b) => a.name.common.localeCompare(b.name.common, 'en-US'))
 
-    const filteredCountries = sortedCountries.filter(({ name }) =>
-        name.common.toLowerCase().includes(serach.toLowerCase()))
+    const filteredCountries = sortedCountries.filter(({ name, region }) => {
+        const nameMatches = name.common.toLowerCase().includes(serach.toLowerCase())
+        const regionMatches = selected === "All regions" || region === selected
+
+        return nameMatches && regionMatches
+    })
 
     return (
         <>
-            <div className="mb-8">
+            <div className="flex justify-between mb-8">
                 <Search
                     count={filteredCountries.length}
                     search={serach}
                     setSearch={setSearch} />
+
+                <Select
+                    options={regions}
+                    selected={selected}
+                    setSelected={setSelected} />
             </div>
             <Grid>
                 {filteredCountries.map(({ cca3, flags, name, capital, region, population }, index) => {
