63a78270b7da287c31c4c0e39bdcb9a3b7c996a4
Matheus Araujo <theusnascimento386@gmail.com>
2025-03-17 21:52:53 -0300

    add some layout improvents

--- app/components/Grid.tsx
+++ app/components/Grid.tsx
@@ -4,7 +4,7 @@ interface GridProps {
 
 const Grid: React.FC<GridProps> = ({ children }) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+    <div className="grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-4 gap-12 lg:gap-8">
       {children}
     </div>
   );
